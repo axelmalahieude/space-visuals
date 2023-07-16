76c60d83e951e024199e279da0247c164978ffe5
Axel Malahieude <axgema@gmail.com>
2023-07-15 18:31:22 -0600

    Convert to using test data

--- server/app.ts
+++ server/app.ts
@@ -1,5 +1,6 @@
 import 'dotenv/config';
-import express, {Express, Request, Response as ExpressResponse} from 'express';
+import express, { Express, Request, Response as ExpressResponse } from 'express';
+import TEST_DATA from '../data/gp_subset.json';
 
 const app: Express = express();
 const port = 3000;
@@ -14,14 +15,15 @@ app.get('/', (_req: Request, res: ExpressResponse) => {
 app.get('/test-fetch', async (_req: Request, res: ExpressResponse) => {
   const username = process.env.SPACE_TRACK_USER;
   const password = process.env.SPACE_TRACK_PASS;
-  const loginRes: Response = await fetch(`https://www.space-track.org/ajaxauth/login`, {
-    method: 'POST',
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded"
-    },
-    body: `identity=${username}&password=${password}&query=https://www.space-track.org/basicspacedata/query/class/gp/orderby/CCSDS_OMM_VERS%20asc/limit/10/emptyresult/show`
-  });
-  const queryData: any = await loginRes.json();
+  // const loginRes: Response = await fetch(`https://www.space-track.org/ajaxauth/login`, {
+  //   method: 'POST',
+  //   headers: {
+  //     "Content-Type": "application/x-www-form-urlencoded"
+  //   },
+  //   body: `identity=${username}&password=${password}&query=https://www.space-track.org/basicspacedata/query/class/gp/orderby/CCSDS_OMM_VERS%20asc/limit/10/emptyresult/show`
+  // });
+  // const queryData: any = await loginRes.json();
+  const queryData = TEST_DATA;
   res.send(queryData);
 });
 
